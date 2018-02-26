74dc66da5114a1d474853e0b8e52c0ca13c138f7
Aditya Verma <adi004.zz@gmail.com>
2018-02-26 23:33:02 +0530

    Initial commit

---
+++ Pixel Art Maker/designs.js
@@ -0,0 +1,13 @@
+// Select color input
+// Select size input
+$("#colorPicker").on("click", function(){
+   var colorPicked = $(this).val();
+   console.log(colorPicked);
+});
+// When size is submitted by the user, call makeGrid()
+
+function makeGrid() {
+
+// Your code goes here!
+
+}
