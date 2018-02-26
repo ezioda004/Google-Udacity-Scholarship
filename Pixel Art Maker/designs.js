1d0fff6ad9f323605a413f6aa69943bd07edb440
Aditya Verma <adi004.zz@gmail.com>
2018-02-27 02:55:29 +0530

    Got the logic working

--- Pixel Art Maker/designs.js
+++ Pixel Art Maker/designs.js
@@ -9,5 +9,34 @@ $("#colorPicker").on("click", function(){
 function makeGrid() {
 
 // Your code goes here!
+let width = $("#inputWeight").val();
+let height = $("#inputHeight").val();
+let colorPicked = $("#colorPicker").val();
+console.log(width);
+
+let rows = "";
+let cols = "";
+for (let i = 1; i <= height; i++){
+    for (let j = 1; j <= width; j++){
+        cols+=`<td></td>`;
+    }
+    rows+=`<tr>${cols}</tr>`
+    cols = "";
 
 }
+console.log(rows);
+$("#pixelCanvas").html(rows);
+
+$("td").on("click", function(){
+    console.log(colorPicked);
+    $(this).css("background", colorPicked);
+})
+
+
+}
+
+$("#submit").on("click", function(e){
+    e.preventDefault();
+    makeGrid();
+});
+
