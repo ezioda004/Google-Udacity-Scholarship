6600377d177bf768597985accb557e2a42cd3fee
Aditya Verma <adi004.zz@gmail.com>
2018-03-02 16:32:07 +0530

    v1.0 Completed for now

--- Pixel Art Maker/designs.js
+++ Pixel Art Maker/designs.js
@@ -1,66 +1,60 @@
-// Select color input
-// Select size input
-// $("h1").addClass("animated fadeIn")
+$("document").ready(function(){
 
-$("#colorPicker").on("click", function(){
-   var colorPicked = $(this).val();
-   console.log(colorPicked);
-});
-// When size is submitted by the user, call makeGrid()
+
+//declared and initialized variables
+let width;
+let height;
 let colorPicked = $("#colorPicker").val();
 
 
-function makeGrid() {
+function makeGrid() { //Grid creating function
 
 // Your code goes here!
-let width = $("#inputWeight").val();
-let height = $("#inputHeight").val();
+    width = $("#inputWeight").val();
+    height = $("#inputHeight").val();
 
-console.log(width);
 
-let rows = "";
-let cols = "";
-let firstRow = 1;;
-for (let i = 1; i <= height; i++){
-    // for (let j = 1; j <= width; j++){
-    //     cols+=`<td></td>`;
-    // }
-    while (width>= firstRow){
-        cols+=`<td></td>`;
-        firstRow+=1;
+
+    let rows = "";
+    let cols = "";
+    let firstRow = 1;;
+    for (let i = 1; i <= height; i++){
+        while (width>= firstRow){  //loop only runs once
+            cols+=`<td></td>`; //a whole row created
+            firstRow+=1;
+        }
+        rows+=`<tr>${cols}</tr>` //pushes cols to each row
     }
-    rows+=`<tr>${cols}</tr>`
-    // cols = "";
+    $("#pixelCanvas").html(rows); //pushes all rows to table
 
-}
-console.log(rows);
-$("#pixelCanvas").html(rows);
+    $("td").on("click", function(){ //event listener for td clicks
+        colorPicked = $("#colorPicker").val();
+        $(this).css("background", colorPicked);
+    });
 
-$("td").on("click", function(){
-    colorPicked = $("#colorPicker").val();
-    console.log(colorPicked);
-    $(this).css("background", colorPicked);
-});
-$("#select").css("top", "0");
-    // fade
-// $("#canvas").fadeIn(2000, function(){
-//     $(this).css("visibility", "visible")
-// }).css("visibility", "visible");
-$("#canvas").css({"opacity": "0","visibility": "visible"}).animate({opacity: 1}, 2000);
+//Animating when the grid function is ran
 
-
+    $("#select").addClass("btn-animate"); 
+    $("#canvas").css({"opacity": "0","visibility": "visible"}).animate({opacity: 1}, 2000);
 }
 
+//Submit button event handler
 $("#submit").on("click", function(e){
     e.preventDefault();
-    // $("#canvas").fadeIn(1500).css("visibility", "visible");
     makeGrid();
    
 });
 
+//Reset button event handler
 $("#reset").on("click", function(e){
     e.preventDefault();
+    $("#inputWeight").val(1);
+    $("#inputHeight").val(1);
+    $("#select").removeClass("btn-animate");
+    $("#pixelCanvas").html("");
+    $("#canvas").css("visibility", "hidden");
+    
 });
 
 
-
+});
