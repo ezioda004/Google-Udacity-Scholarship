58f21c9192050e5e83db856bafafa17081adbb74
Aditya Verma <adi004.zz@gmail.com>
2018-06-30 18:17:41 +0530

    feat: added character selection option

--- Arcade Game/js/app.js
+++ Arcade Game/js/app.js
@@ -1,6 +1,5 @@
 // Enemies our player must avoid
 
-
 // The Enemy function, which initiates the Enemy by:
 // Loading the image by setting this.sprite to the appropriate image in the image folder (already provided)
 // Setting the Enemy initial location (you need to implement)
@@ -12,23 +11,27 @@
 
 class Enemy {
     constructor(x, y) {
-        this.sprite = 'images/enemy-bug.png';
+        this.sprite = "images/enemy-bug.png";
         this.x = x;
         this.y = y;
-        this.speed = Math.floor((Math.random() * (250 - 100)) + 100);
+        this.speed = Math.floor(Math.random() * (250 - 100) + 100);
     }
     update(dt) {
         if (this.x >= 500) {
             this.x = 1;
         } else {
-            this.x += (dt * this.speed);
+            this.x += dt * this.speed;
         }
         this.collision();
     }
     collision() {
-
-        if ((((this.x + 70) >= player.x) && ((this.x - 70) <= player.x)) && ((this.y + 30) >= player.y) && ((this.y - 50) <= player.y)) {
-            console.log("collided")
+        if (
+            this.x + 70 >= player.x &&
+            this.x - 70 <= player.x &&
+            this.y + 30 >= player.y &&
+            this.y - 50 <= player.y
+        ) {
+            console.log("collided");
             player.x = 200;
             player.y = 450;
         }
@@ -38,11 +41,9 @@ class Enemy {
     }
 }
 
-
-
 class Player {
     constructor() {
-        this.sprite = 'images/char-boy.png';
+        this.sprite = "images/char-boy.png"
         this.x = 200;
         this.y = 450;
     }
@@ -53,9 +54,10 @@ class Player {
                 this.y = 450;
             }, 50);
         }
-
     }
     render() {
+        this.sprite = this.sprite.replace(/^\.\//, "");
+        // this.sprite = "images/char-cat-girl.png";
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
     handleInput(key) {
@@ -67,7 +69,6 @@ class Player {
             } else {
                 this.y += 50;
             }
-
         } else if (key == "right") {
             this.x += 50;
         }
@@ -75,9 +76,7 @@ class Player {
             this.x -= 50;
         }
         if (this.x >= 420 || this.x <= -25) {
-            Math.sign(this.x) == 1 ? this.x -= 450 : this.x += 450;
-
-
+            Math.sign(this.x) == 1 ? (this.x -= 450) : (this.x += 450);
         }
     }
 }
@@ -94,23 +93,27 @@ allEnemies = [e1, e2];
 console.log(allEnemies);
 player = p1;
 
-
-
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
-
-
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function (e) {
+document.addEventListener("keyup", function (e) {
     var allowedKeys = {
-        37: 'left',
-        38: 'up',
-        39: 'right',
-        40: 'down'
+        37: "left",
+        38: "up",
+        39: "right",
+        40: "down"
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
+
+window.onload = function () {
+    document.querySelector(".avatar").addEventListener("click", function (e) {
+        e.stopPropagation();
+        console.log(e.target.getAttribute("src").replace(/^\.\//, ""), player.sprite);
+        player.sprite = "./" + e.target.getAttribute("src").replace(/^\.\//, "");
+    }, true);
+};
