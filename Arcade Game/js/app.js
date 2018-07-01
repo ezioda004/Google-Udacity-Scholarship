0fcb72ffbbda817e81f196969b38803d8615bfb4
Aditya Verma <adi004.zz@gmail.com>
2018-07-01 15:52:14 +0530

    feat: completed the game, added random items that spawn and enemies as well as modal and relevant comments

--- Arcade Game/js/app.js
+++ Arcade Game/js/app.js
@@ -1,21 +1,16 @@
-// Enemies our player must avoid
+//Score node selector
+const score = document.querySelector(".score");
 
-// The Enemy function, which initiates the Enemy by:
-// Loading the image by setting this.sprite to the appropriate image in the image folder (already provided)
-// Setting the Enemy initial location (you need to implement)
-// Setting the Enemy speed (you need to implement)
-// The update method for the Enemy
-// Updates the Enemy location (you need to implement)
-// Handles collision with the Player (you need to implement)
-// You can add your own Enemy methods as needed
-
+//Enemy Class
 class Enemy {
     constructor(x, y) {
         this.sprite = "images/enemy-bug.png";
         this.x = x;
         this.y = y;
+        //Random speed when instansciated
         this.speed = Math.floor(Math.random() * (250 - 100) + 100);
     }
+    //Update method that updates the movement
     update(dt) {
         if (this.x >= 500) {
             this.x = 1;
@@ -24,23 +19,25 @@ class Enemy {
         }
         this.collision();
     }
+    //Checking if collison happened
     collision() {
-        if (
-            this.x + 70 >= player.x &&
-            this.x - 70 <= player.x &&
-            this.y + 30 >= player.y &&
-            this.y - 50 <= player.y
-        ) {
-            console.log("collided");
+        if (this.x + 70 >= player.x && this.x - 70 <= player.x && this.y + 30 >= player.y && this.y - 50 <= player.y) {
             player.x = 200;
             player.y = 450;
+            const modal = document.querySelector(".modal");
+            modal.children[0].children[1].textContent = "You Lost!";
+            modal.classList.remove("hide");
+            score.textContent = 0;
+            allEnemies = [];
         }
     }
+    //Render on canvas
     render() {
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
 }
 
+//Player class
 class Player {
     constructor() {
         this.sprite = "images/char-boy.png"
@@ -48,18 +45,22 @@ class Player {
         this.y = 450;
     }
     update(dt) {
+        //winning condition
         if (this.y == 0) {
             setTimeout(() => {
                 this.x = 200;
                 this.y = 450;
+                score.textContent = +score.textContent + 5;
+                game = new Game();
+                allEnemies = [new Enemy(0, 50), new Enemy(0, 140), new Enemy(0, 225), new Enemy(0, 50), new Enemy(0, 140), new Enemy(0, 225)];
             }, 50);
         }
     }
     render() {
         this.sprite = this.sprite.replace(/^\.\//, "");
-        // this.sprite = "images/char-cat-girl.png";
         ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     }
+    //For controlling with keyboard
     handleInput(key) {
         if (key == "up") {
             this.y -= 50;
@@ -75,30 +76,81 @@ class Player {
         if (key == "left") {
             this.x -= 50;
         }
+
+        //Offscreen prevent
         if (this.x >= 420 || this.x <= -25) {
             Math.sign(this.x) == 1 ? (this.x -= 450) : (this.x += 450);
         }
     }
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+//Game class
+class Game {
+    constructor() {
+        this.gemBlue = this.coords();
+        this.gemBlue.sprite = "images/Gem Blue.png";
 
+        this.gemGreen = this.coords();
+        this.gemGreen.sprite = "images/Gem Green.png"
+
+        this.gemOrange = this.coords();
+        this.gemOrange.sprite = "images/Gem Orange.png"
+
+        this.rock = this.coords();
+        this.rock.sprite = "images/Rock.png"
+    }
+    coords() {
+        //Generating random coordinates for items
+        let x = [15, 115, 215, 315, 415];
+        let y = [90, 175, 260, 345];
+        return [x[Math.floor(Math.random() * 5)], y[Math.floor(Math.random() * 4)]];
+    }
+    collision() {
+        if (this.gemBlue[0] + 70 >= player.x && this.gemBlue[0] - 70 <= player.x && this.gemBlue[1] + 30 >= player.y && this.gemBlue[1] - 50 <= player.y) {
+            this.gemBlue[0] = -100;
+            this.gemBlue[1] = -1000;
+            score.textContent = +score.textContent + 5;
+        }
+        if (this.gemGreen[0] + 70 >= player.x && this.gemGreen[0] - 70 <= player.x && this.gemGreen[1] + 30 >= player.y && this.gemGreen[1] - 50 <= player.y) {
+            this.gemGreen[0] = -100;
+            this.gemGreen[1] = -1000;
+            score.textContent = +score.textContent + 10;
+        }
+        if (this.gemOrange[0] + 70 >= player.x && this.gemOrange[0] - 70 <= player.x && this.gemOrange[1] + 30 >= player.y && this.gemOrange[1] - 50 <= player.y) {
+            this.gemOrange[0] = -100;
+            this.gemOrange[1] = -1000;
+            score.textContent = +score.textContent + 15;
+        }
+        if (this.rock[0] + 70 >= player.x && this.rock[0] - 70 <= player.x && this.rock[1] + 30 >= player.y && this.rock[1] - 50 <= player.y) {
+            this.rock[0] = -100;
+            this.rock[1] = -1000;
+            score.textContent = +score.textContent - 30;
+        }
+
+    }
+    render() {
+        ctx.drawImage(Resources.get(this.gemBlue.sprite), this.gemBlue[0], this.gemBlue[1]);
+        ctx.drawImage(Resources.get(this.gemGreen.sprite), this.gemGreen[0], this.gemGreen[1]);
+        ctx.drawImage(Resources.get(this.gemOrange.sprite), this.gemOrange[0], this.gemOrange[1]);
+        ctx.drawImage(Resources.get(this.rock.sprite), this.rock[0], this.rock[1]);
+        this.collision();
+    }
+}
+
+//Instancing from Enemey, Game and Player class
 let allEnemies, player;
 let e1 = new Enemy(0, 50);
 let e2 = new Enemy(0, 225);
+let e3 = new Enemy(0, 140);
+
 let p1 = new Player();
-allEnemies = [e1, e2];
-console.log(allEnemies);
+let game = new Game();
+allEnemies = [e1, e2, e3];
 player = p1;
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+
 
 // This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
 document.addEventListener("keyup", function (e) {
     var allowedKeys = {
         37: "left",
@@ -111,9 +163,14 @@ document.addEventListener("keyup", function (e) {
 });
 
 window.onload = function () {
+    //Event to select avatar
     document.querySelector(".avatar").addEventListener("click", function (e) {
         e.stopPropagation();
-        console.log(e.target.getAttribute("src").replace(/^\.\//, ""), player.sprite);
         player.sprite = "./" + e.target.getAttribute("src").replace(/^\.\//, "");
     }, true);
+    //Event to close the modal
+    document.querySelector(".close").addEventListener("click", function () {
+        document.querySelector(".modal").classList.add("hide");
+        allEnemies = [new Enemy(0, 50), new Enemy(0, 140), new Enemy(0, 225), new Enemy(0, 50), new Enemy(0, 140), new Enemy(0, 225)];
+    })
 };
--- Arcade Game/js/engine.js
+++ Arcade Game/js/engine.js
@@ -154,6 +154,7 @@ var Engine = (function(global) {
         });
 
         player.render();
+        game.render();
     }
 
     /* This function does nothing but it could have been a good place to
@@ -177,7 +178,11 @@ var Engine = (function(global) {
         'images/char-cat-girl.png',
         'images/char-horn-girl.png',
         'images/char-pink-girl.png',
-        'images/char-princess-girl.png'
+        'images/char-princess-girl.png',
+        'images/Gem Blue.png',
+        'images/Gem Green.png',
+        'images/Gem Orange.png',
+        'images/Rock.png'
     ]);
     Resources.onReady(init);
 
