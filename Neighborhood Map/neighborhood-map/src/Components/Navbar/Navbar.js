9eda29f6d9a4788cc453604e15b16bc0cb4c2a00
Aditya Verma <adi004.zz@gmail.com>
2018-07-22 15:04:56 +0530

    bug: hamburger changes to cross on click and vice versa

--- Neighborhood Map/neighborhood-map/src/Components/Navbar/Navbar.js
+++ Neighborhood Map/neighborhood-map/src/Components/Navbar/Navbar.js
@@ -2,12 +2,27 @@ import React, { Component } from "react";
 import "./Navbar.css";
 
 class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isOpen: false
+    };
+  }
+  onClickHandler = () =>{
+    this.setState(prevState => ({
+        isOpen: !prevState.isOpen
+    }))
+    this.props.listViewOpenHandler()
+  }
   render() {
     return (
       <nav>
         <div className="nav-drawer">
-          <a className="canvas-toggler" onClick = {this.props.listViewOpenHandler}>
-            <i className="fas fa-bars" />
+          <a
+            className="canvas-toggler"
+            onClick={this.onClickHandler}
+          >
+            <i className= {`fas ${this.state.isOpen ? `fa-times` : `fa-bars`}`} />
           </a>
         </div>
         <div className="heading">
